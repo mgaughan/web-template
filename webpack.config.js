dc732fdb16ae0f171d175786e6883257efd28e95
Mike Gaughan <michael.t.gaughan@gmail.com>
2017-02-18 00:27:33 -0700

    Few fixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/app.entry.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
@@ -30,7 +30,7 @@ module.exports = {
       },
       {
         enforce: 'pre',
-        test: /\.js$/, 
+        test: /\.(js|vue)$/, 
         loader: "eslint-loader", 
         exclude: /node_modules/
       },
